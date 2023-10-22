7db92bbdbcc10738cee7c43a90f5bfa9900a2772
Enrique <Enrique@DESKTOP-8KDOF0O>
2023-10-22 17:51:53 +0200

    Comitt ventanas 2

--- Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaInicio.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaInicio.java
@@ -10,32 +10,47 @@ import java.awt.*;
 public class VentanaInicio extends JFrame{
 	private JButton btnInicioSesion, btnRegistroTrabajador, btnRegistroPaciente, btnSalir;
 	private JPanel  pSur, pCentro ,pCentroIzq, pCentroDcha;
-	
+	private JFrame vActual;
 	
 	/*Creamos la primera ventana que se viusualiza*/
 	public VentanaInicio()
 	{	
+		vActual = this;
+		setTitle("Bienvenido a Furwell!");
 		setBounds(400, 100, 800, 600);
 		setVisible(true);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		/*Creamos paneles y componentes*/
-		pCentro = new JPanel(new GridLayout(1,2));
-		pCentroIzq = new JPanel(new GridLayout(2, 2));
-		pCentroDcha = new JPanel(new GridLayout(4, 2));
+		pCentro = new JPanel();
+		pCentroIzq = new JPanel();
+		pCentroDcha = new JPanel();
+		pSur = new JPanel();		
 		
-		pCentroDcha.setBackground(Color.RED);
 		pCentro.add(pCentroIzq);
 		pCentro.add(pCentroDcha);
-		
-		getContentPane().add(pCentro, BorderLayout.CENTER);
-		
-		
 		btnInicioSesion = new JButton("Iniciar Sesion");
 		btnRegistroTrabajador = new JButton("Registro (Trabajador)");
 		btnRegistroPaciente = new JButton("Registro (Paciente)");
+		btnSalir = new JButton("Salir");
 		pCentroDcha.add(btnRegistroTrabajador);
 		pCentroDcha.add(btnRegistroPaciente);
 		pCentroIzq.add(btnInicioSesion);
+		pSur.add(btnSalir);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		
+		
+		/*Eventos*/
+		
+		btnSalir.addActionListener((e)->{
+			System.exit(0);
+			
+		});
+		
+		btnInicioSesion.addActionListener((e)->{
+			
+			/*Preguntar como abrir una ventana al clickar el boton*/
+		});
 
 	}
 	
--- Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaRegistroTrabajador.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaRegistroTrabajador.java
@@ -3,6 +3,8 @@ package ventanas;
 import javax.swing.*;
 import javax.swing.JFrame;
 
+import domain.Paciente;
+
 public class VentanaRegistroTrabajador extends JFrame 
 {
 	private JPanel PanelNorte, PanelSur, PanelCentro, PanelCentroDcha, PanelCentroIzq;
@@ -10,6 +12,24 @@ public class VentanaRegistroTrabajador extends JFrame
 	private JTextField txtNomR, txtDniR, txtApeR, txtUsuR;
 	private JPasswordField txtConR;
 	private JButton btnRegistrarse, btnSalir;
+	private JFrame vActual;
+	
+	private static Paciente paciente;
+	
+	public static Paciente getPaciente() {
+		return paciente;
+	}
+	
+	
+	
+	public VentanaRegistroTrabajador() {
+		super();
+		vActual = this;
+		setBounds(400, 100, 800, 500);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+	}
 	
 	
 	
