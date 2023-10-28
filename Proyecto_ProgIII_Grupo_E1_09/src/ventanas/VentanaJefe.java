52e0e03b53fdcdd7fb9fedeb2884d8dfb2dc63bd
Hugo Arenaza <hugo.arenaza@opendeusto.es>
2023-10-28 18:58:29 +0200

    Creacion Ventana Jefe

--- Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaJefe.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaJefe.java
@@ -1,5 +1,34 @@
 package ventanas;
 
-public class VentanaJefe {
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VentanaJefe extends JFrame {
+public VentanaJefe() {
+	setTitle("Ventana Jefe");
+	setBounds(400, 100, 800, 600);
+	setVisible(true);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);	
+
+	JPanel panelArriba= new JPanel();
+
+	JButton botonClinicas = new JButton("Clinicas");
+	panelArriba.add(botonClinicas);
+	
+	getContentPane().add(panelArriba,BorderLayout.NORTH);
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+}
 }
