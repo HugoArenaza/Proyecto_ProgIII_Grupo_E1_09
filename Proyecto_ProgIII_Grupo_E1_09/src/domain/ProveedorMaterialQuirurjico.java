f2b62cb91d1fbc1bcf9c95a7e55c2c6f4cfe515d
Hugo Arenaza <hugo.arenaza@opendeusto.es>
2023-10-31 14:56:26 +0100

    Cambio desde medicamentes a materiales

--- Proyecto_ProgIII_Grupo_E1_09/src/domain/ProveedorMaterialQuirurjico.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/domain/ProveedorMaterialQuirurjico.java
@@ -2,7 +2,30 @@ package domain;
 
 public class ProveedorMaterialQuirurjico extends Proveedor {
 	private TipoMaterialQuirurjico tipoMaterialQuirurjico;
-
+	
+	/*BISTURI,
+    TIJERAS_DE_CIRUGIA,
+    PINZAS,
+    JERINGAS_Y_AGUJAS,
+    GUANTES_QUIRURGICOS,
+    MASCARILLAS_Y_GAFAS_DE_PROTECCION,
+    CAMPO_QUIRURGICO,
+    GASAS_Y_COMPRESAS,
+    SUTURAS_Y_MATERIAL_DE_LIGADURA,
+    TUBOS_ENDOTRAQUEALES,
+    CANULAS_DE_TRAQUEOTOMIA,
+    ELECTROBISTURI,
+    AGUJAS_DE_BIOPSIA,
+    PORTAAGUJAS,
+    HOJAS_DE_SCALPEL,
+    INSTRUMENTAL_DE_CIRUGIA_LAPAROSCOPICA,
+    DRENAJES_QUIRURGICOS,
+    BOLSAS_DE_ESTERILIZACION,
+    CANULAS_INTRAVENOSAS_IV,
+    VENDAS_Y_APOSITOS
+	 */
+	
+	
 	public ProveedorMaterialQuirurjico(String correo, int num_contacto, TipoMaterialQuirurjico tipoMaterialQuirurjico) {
 		super(correo, num_contacto);
 		this.tipoMaterialQuirurjico = tipoMaterialQuirurjico;
