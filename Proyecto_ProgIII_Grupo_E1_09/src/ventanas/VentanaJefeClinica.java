077739cd3ed1b65187bc37d3a42bcf1992b3aad4
Enrique Corres <enrique.corres@opendeusto.es>
2023-11-03 09:56:29 +0100

    Creacion evento btnSalir en Ventana Jefe CLinica, Bounds y new JPanels

--- Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaJefeClinica.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/ventanas/VentanaJefeClinica.java
@@ -1,5 +1,7 @@
 package ventanas;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,9 +12,36 @@ public class VentanaJefeClinica extends JFrame{
 	
 	
 	public VentanaJefeClinica() {
-		setBounds(400, 100, 800, 500);
-		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setBounds(400, 100, 800, 500);
+		setTitle("Jefe de CLinica");
+		pNorte = new JPanel();
+		pSur  = new JPanel();
+		pCentro = new JPanel();
+		
+		pSur.add(btnSalir);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		
+		
+		/*Eventos*/
+		btnSalir.addActionListener((e)->{
+			dispose();
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		setVisible(true);
 		
 	}
 
