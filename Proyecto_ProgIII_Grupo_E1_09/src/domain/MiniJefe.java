e8671c8d51025f467f0dae308ac5f8a1e90e06f4
Hugo Arenaza <hugo.arenaza@opendeusto.es>
2023-11-07 22:53:38 +0100

    Implementacion jerarquia con Jefe, MiniJefe, trabjador y Veterinario

--- Proyecto_ProgIII_Grupo_E1_09/src/domain/MiniJefe.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/domain/MiniJefe.java
@@ -1,10 +1,33 @@
 package domain;
 
-public class MiniJefe extends Veterinario {
-	private int num_empleados;
-	private int id_clinica;
-	
-	
-	
+import java.util.ArrayList;
 
+public class MiniJefe extends Trabajador {
+	private int num_empleados;
+
+	public MiniJefe(String nombre, String apellidos, String usuario, String contraseña, int id_clinica,
+			ArrayList<Paciente> pacientes, Float sueldo, Especialidades especialidad, String dni, int num_pacientes,
+			int num_empleados) {
+		super(nombre, apellidos, usuario, contraseña, id_clinica, pacientes, sueldo, especialidad, dni, num_pacientes);
+		this.num_empleados = num_empleados;
+	}
+
+	public int getNum_empleados() {
+		return num_empleados;
+	}
+
+	public void setNum_empleados(int num_empleados) {
+		this.num_empleados = num_empleados;
+	}
+
+	@Override
+	public String toString() {
+		return "MiniJefe [num_empleados=" + num_empleados + ", getNum_pacientes()=" + getNum_pacientes()
+				+ ", toString()=" + super.toString() + ", getDni()=" + getDni() + ", getNombre()=" + getNombre()
+				+ ", getApellidos()=" + getApellidos() + ", getUsuario()=" + getUsuario() + ", getContraseña()="
+				+ getContraseña() + ", getClinica()=" + getClinica() + ", getPaciente()=" + getPaciente()
+				+ ", getSueldo()=" + getSueldo() + ", getespecialidad()=" + getespecialidad() + "]";
+	}
+
+	
 }
--- Proyecto_ProgIII_Grupo_E1_09/src/domain/Trabajador.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/domain/Trabajador.java
@@ -1,5 +1,40 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Trabajador extends Veterinario{
+	private int num_pacientes;
+	
+	
+	
+	
+		public int getNum_pacientes() {
+		return num_pacientes;
+	}
+
+	public void setNum_pacientes(int num_pacientes) {
+		this.num_pacientes = num_pacientes;
+	}
+
+	public Trabajador(String nombre, String apellidos, String usuario, String contraseña, int id_clinica,
+			ArrayList<Paciente> pacientes, Float sueldo, Especialidades especialidad, String dni, int num_pacientes) {
+		super(nombre, apellidos, usuario, contraseña, id_clinica, pacientes, sueldo, especialidad, dni);
+		this.num_pacientes = num_pacientes;
+	}
+
+	@Override
+	public String toString() {
+		return "Trabajador [num_pacientes=" + num_pacientes + ", getDni()=" + getDni() + ", getNombre()=" + getNombre()
+				+ ", getApellidos()=" + getApellidos() + ", getUsuario()=" + getUsuario() + ", getContraseña()="
+				+ getContraseña() + ", getClinica()=" + getClinica() + ", getPaciente()=" + getPaciente()
+				+ ", getSueldo()=" + getSueldo() + ", getespecialidad()=" + getespecialidad() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	
+	
+	
+	
+	
 
 }
