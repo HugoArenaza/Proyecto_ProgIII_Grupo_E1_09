63d34c39177e7689395c7cb501c9574a6d4477e7
Enrique Corres <enrique.corres@opendeusto.es>
2023-12-30 19:41:20 +0100

    Nuevos test de la Base de Datos

--- Proyecto_ProgIII_Grupo_E1_09/src/base_de_datos/BDTest.java
+++ Proyecto_ProgIII_Grupo_E1_09/src/base_de_datos/BDTest.java
@@ -1,4 +1,4 @@
-/*package base_de_datos;
+package base_de_datos;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
@@ -9,10 +9,33 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import domain.Paciente;
+
 
 
 public class BDTest {
 	private Connection con;
+	@Before
+	public void setUp()throws Exception{
+		con = BD.initBD("clinicaFurwell.db");
+	}
 	
+	@After 
+	public void tearDown() throws Exception{
+		BD.cerrarBD(con);
+	}
 	
-}*/
+	@Test
+	public void testCrearTablas() {
+		assertThrows(AssertionError.class, ()->{
+			assertThrows(SQLException.class, ()->{BD.crearTablas(con);});
+		});
+	}
+	
+	/*@Test
+	public void testInsertarPaciente() {
+		Paciente p = new Paciente(0, null, 0, null, 0, null, null);
+		BD.insertarPaciente(con,"Sin NombreDueño", p);
+	}*/
+	
+}
